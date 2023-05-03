06af2cf1b337565ceedb44ace80fd3d4f0e78562
Dante Expósito <dante0im@gmail.com>
2023-05-03 18:15:18 -0300

    Finished App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const todo = e.target.elements[0];
+
+    setTodos([
+      ...todos,
+      { id: Date.now(), text: todo.value, completed: false },
+    ]);
+
+    todo.value = '';
+  };
+
+  const handleToggle = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+  const handleDelete = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <form onSubmit={handleSubmit}>
+        <input type="text" />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos &&
+          todos.map((todo, i) => (
+            <li
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none',
+              }}
+              key={i}
+            >
+              <span onClick={() => handleToggle(todo.id)}>{todo.text}</span>
+              <button
+                type="button"
+                style={{ marginLeft: '1rem' }}
+                onClick={() => handleDelete(todo.id)}
+              >
+                X
+              </button>
+            </li>
+          ))}
+      </ul>
+    </main>
+  );
 }
 
-export default App
+export default App;
